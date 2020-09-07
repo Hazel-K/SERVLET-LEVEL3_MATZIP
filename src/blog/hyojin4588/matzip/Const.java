f80af5edb314167459cdccedbadf286fc5f03791
Hazel_K <hyojin4588@naver.com>
2020-09-07 14:43:25 +0900

    프로젝트 내 자주 사용하는 상수 지정

---
+++ src/blog/hyojin4588/matzip/Const.java
@@ -0,0 +1,7 @@
+package blog.hyojin4588.matzip;
+
+public class Const {
+	public static final String TEMPLATE = "template";
+	public static final String VIEW = "view";
+	public static final String TITLE = "title";
+}
