1b2751b5426e9507a73a92c285dda45f94374256
Hazel_K <hyojin4588@naver.com>
2020-09-07 17:56:16 +0900

    마리아 DB 접속 DAO 생성

---
+++ src/blog/hyojin4588/matzip/db/DbManager.java
@@ -0,0 +1,32 @@
+package blog.hyojin4588.matzip.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbManager {
+	
+	// test용 메소드. java파일 우클릭 - run as - java application으로 실행 후 결과 확인
+//	public static void main(String args[]) {
+//		try {
+//			getCon();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+	// test용 메소드. java파일 우클릭 - run as - java application으로 실행 후 결과 확인
+	
+	public static Connection getCon() throws Exception {
+		// mysql DB 접속
+		String url = "jdbc:mysql://127.0.0.1:3306/matzip";
+		String user = "root";
+		String pw = "koreait2020";
+		String className = "com.mysql.cj.jdbc.Driver"; // library path
+		
+		Class.forName(className);
+		Connection con = DriverManager.getConnection(url, user, pw);
+		System.out.println("DB 연결 완료");
+		
+		return con;
+		// mysql DB 접속
+	}
+}
