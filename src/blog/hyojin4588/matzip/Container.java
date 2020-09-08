af2b1e5e2051735470602c9e429ae540a92e57e8
Hazel_K <hyojin4588@naver.com>
2020-09-08 09:55:14 +0900

    test case 추가

--- src/blog/hyojin4588/matzip/Container.java
+++ src/blog/hyojin4588/matzip/Container.java
@@ -43,10 +43,12 @@ public class Container extends HttpServlet {
 	
 	private void proc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String temp = mapper.nav(request); // URI 주소 받기
-		System.out.println("temp : " + temp);
+//		System.out.println("temp : " + temp);
+//		System.out.println("redirect:/user/login".substring("redirect:/user/login".indexOf("/")));
+//		System.out.println("redirect:/user/login".substring(0, "redirect:/user/login".indexOf("/")));
 		
 		// temp안에 "/"가 존재하고, redirect:라는 글자가 있다면 redirect라고 적힌 다음 경로를 추출해 그 페이지로 이동 <시작>
-		if(temp.indexOf("/") >=0 && "redirect:".equals(temp.substring(0, temp.indexOf("/")))) {			
+		if(temp.indexOf("/") >=0 && "redirect:".equals(temp.substring(0, temp.indexOf("/")))) {
 			response.sendRedirect(temp.substring(temp.indexOf("/")));
 			return;
 		}
