8866a1334c2dfc641c4d8e10c9cc098545365cd6
Hazel_K <hyojin4588@naver.com>
2020-09-08 10:56:54 +0900

    비밀번호 암호화 작업 완료

---
+++ src/blog/hyojin4588/matzip/SecurityUtils.java
@@ -0,0 +1,57 @@
+package blog.hyojin4588.matzip;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class SecurityUtils {
+	
+	public static String getEncrypt(String source, String salt) {
+        return getEncrypt(source, salt.getBytes());
+    }
+    
+    public static String getEncrypt(String source, byte[] salt) {
+        String result = "";
+        
+        byte[] a = source.getBytes();
+        byte[] bytes = new byte[a.length + salt.length];
+        
+        System.arraycopy(a, 0, bytes, 0, a.length);
+        System.arraycopy(salt, 0, bytes, a.length, salt.length);
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(bytes);
+            
+            byte[] byteData = md.digest();
+            
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xFF) + 256, 16).substring(1));
+            }
+            
+            result = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+
+	public static String generateSalt() {
+		Random random = new Random(); // 난수를 지정하는 랜덤 객체 생성
+		byte[] salt = new byte[8]; // 크기가 8인 byte 배열 생성
+		random.nextBytes(salt); // 제공받은 salt Array의 크기만큼의 난수를 생성
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < salt.length; i++) {
+			// byte 값을 hex로 바꾸기
+			System.out.println(String.format("%d : %s", i, salt[i]));
+			sb.append(String.format("%02x", salt[i]));
+		}
+		
+		return sb.toString();
+	}
+
+// 출처: https://javannspring.tistory.com/105 [JiGyeong's study room]
+}
